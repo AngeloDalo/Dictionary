2c6021f0642fd1a4787233567e852a15c25ae347
Angelo <Angelo@DESKTOP-6JKRPIT>
2023-05-03 16:33:39 +0200

    Aggiungere i file di progetto.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Corso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DICTIONARY come hashtable ma bisogna definire chiave (non possono essere generiche)
+            Dictionary<string, string> prova = new Dictionary<string, string>()
+            {
+                {"Lombardia", "milano" },
+                {"Liguria", "genova" },
+                {"Piemonte", "torino" }
+            };
+            prova.Add("Veneto", "venezia");
+            prova["Veneto"] = "palermo";
+            prova.Remove("Veneto");
+
+            foreach(KeyValuePair<string, string> item in prova)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
+            }
+
+            prova.Clear();
+        }
+    }
+}
